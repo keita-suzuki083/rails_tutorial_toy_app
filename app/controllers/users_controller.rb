17fd06353879d1fe1d4bb730f36fa4d7d4f9d16d
keita_suzuki <suke083@gmail.com>
2018-05-03 13:56:00 +0900

    fix users_controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @microposts = @user.microposts
+    # @microposts = @user.microposts
   end
 
   # GET /users/new
